6cae39b94efc892f12da15254fb9d605c13b7e37
Muhammad-Yousuf3 <coolmaster808@gmail.com>
2024-11-09 01:58:51 +0500

    Portfolio

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link"
+
+
+function Contact(){
+
+    return(
+        <main className="flex">
+            
+        <section className="min-h-screen w-[40%] md:w-[30%] bg-purple-600 rounded-tr-xl rounded-br-xl"></section>
+
+        <section className="md:w-[50%] w-[100%] h-2/4 rounded-xl bg-zinc-800 md:mt-24 mt-44 md:-ml-48 -ml-36">
+            <h2 className="font-bold text-4xl text-white pt-5 pl-5">Connect With Me</h2>
+
+            <section className="flex flex-col gap-y-8 ml-10 mt-5">
+
+
+                <section className="flex gap-x-7">
+                <i className="fa fa-map-marker text-white text-3xl hover:text-purple-600"></i>
+                <p className="text-white border-b-2 text-lg">Karachi Pakistan, Gulshan-e-Iqbal Block 10-A</p>
+                </section>
+
+                <section className="text-white flex gap-x-7">
+                <i className="fa fa-phone text-3xl hover:text-purple-600"></i>
+                <p className="border-b-2 text-lg">0311-2619419</p>
+                </section>
+
+                <section>
+                <Link className="flex gap-x-7 font-bold" href={"mailto:coolmaster808@gmail.com"}>
+                <i className="fa fa-envelope text-white text-3xl hover:text-purple-600"></i>
+                <p className="text-white border-b-2 text-lg">coolmaster808@gmail.com</p></Link>
+                </section>
+
+
+            </section>
+
+            <h2 className="text-purple-600 text-4xl font-bold pl-5 pt-7">Follow Me</h2>
+            <section className="ml-12 mt-3 mb-3">
+            <Link href={"https://www.linkedin.com/in/muhammad-yousuf-786yousuf/"}><i className="fab fa-linkedin  text-gray-500 text-5xl hover:transition-transform hover:scale-110 hover:text-purple-500 "></i></Link>
+            <Link href={"https://github.com/Muhammad-Yousuf3"}><i className="fab fa-github  text-gray-500 text-5xl mx-5 rounded-full hover:transition-transform hover:scale-110 hover:text-purple-500"></i></Link>
+            </section>
+        
+        
+        </section>
+            
+        </main>
+    )
+}
+export default Contact
--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link"
 function Myfooter () {
     return(
-    <footer className="bg-slate-200 flex justify-between h-[80px]">
-         <h2 className="text-[#1B263B] text-3xl font-bold my-5 px-10">Portfolio</h2>
-
-        <section className="my-5">
-        <h3 className="text-[#1B263B] text-xl font-semibold py-2">Let&apos;s Build Together.</h3>
-        </section>
-
-        <section className="my-5 px-10">
-        <Link href={"https://www.linkedin.com/in/muhammad-yousuf-786yousuf/"}><i className="fab fa-linkedin bg-slate-200 text-[#0A66C2] text-[48px] "></i></Link>
-    <Link href={"https://github.com/Muhammad-Yousuf3"}><i className="fab fa-github bg-slate-200 text-black text-[48px] mx-5 rounded-full"></i></Link>
-        </section>
+    <footer className="md:fixed md:bottom-0 bg-zinc-900 border-t-2  border-gray-500 flex flex-col sm:flex-row justify-between items-center gap-10 h-auto sm:h-24 w-full ">
+         
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
+        
+        <h2 className="text-white text-3xl font-bold px-10">Portfolio</h2>
+        <h3 className="text-gray-500 text-xl font-semibold ">Let&apos;s Build Together.</h3>
+        
+       
+        
+    <section className="mr-5">
+    <Link href={"https://www.linkedin.com/in/muhammad-yousuf-786yousuf/"}><i className="fab fa-linkedin  text-gray-500 text-5xl hover:transition-transform hover:scale-110 hover:text-purple-500"></i></Link>
+    <Link href={"https://github.com/Muhammad-Yousuf3"}><i className="fab fa-github  text-gray-500 text-5xl mx-5 rounded-full hover:transition-transform hover:scale-110 hover:text-purple-500"></i></Link>
+    <Link href={"mailto:coolmaster808@gmail.com"}><i className="fa fa-envelope text-gray-500 text-5xl hover:transition-transform hover:scale-110 hover:text-purple-500"></i></Link>
+    </section>
+        
     </footer>
 )}
 export default Myfooter
